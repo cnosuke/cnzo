ead70dcb4f411843a6265a045601107ac68c4f9a
cnosuke <shinnosuke@gmail.com>
2015-08-13 18:09:58 +0900

    Fix run scripta

--- app/run_on_docker.sh
+++ app/run_on_docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /app && RBENV_ROOT=~/.rbenv RBENV_VERSION=2.2.2 ~/.rbenv/bin/rbenv exec bundle exec unicorn -E production -c config/unicorn.rb
+cd /app && bundle exec unicorn -E production -c config/unicorn.rb
